7891e4df4ced48aad25f9b8caf9d1331d95bf440
Ren Shao <ren@shao.io>
2014-03-10 23:01:53 +1100

    should not rev CNAME file

--- Gruntfile.js
+++ Gruntfile.js
@@ -221,8 +221,7 @@ module.exports = function (grunt) {
                         '<%= config.dist %>/styles/{,*/}*.css',
                         '<%= config.dist %>/images/{,*/}*.*',
                         '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}',
-                        '<%= config.dist %>/CNAME'
+                        '<%= config.dist %>/*.{ico,png}'
                     ]
                 }
             }
