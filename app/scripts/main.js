45346b22eab707de3eac142797b276bb12df37de
Ren Shao <ren@shao.io>
2014-03-11 15:32:35 +1100

    beta product site taking shape

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,9 @@
-var dataRef = new Firebase("https://burning-fire-8925.firebaseio.com");
-var emailsRef = dataRef.child('emails');
+// var dataRef = new Firebase("https://burning-fire-8925.firebaseio.com");
+// var emailsRef = dataRef.child('emails');
 
-function saveEmail() {
-  var email = document.querySelector('#email').value;
-  if (email) {
-    emailsRef.push({email: email, timestamp: Firebase.ServerValue.TIMESTAMP});
-  }
-}
+// function saveEmail() {
+//   var email = document.querySelector('#email').value;
+//   if (email) {
+//     emailsRef.push({email: email, timestamp: Firebase.ServerValue.TIMESTAMP});
+//   }
+// }
