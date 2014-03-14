bbdfc14262474bb2692cdcefc094f79560b2d535
Ren Shao <ren@shao.io>
2014-03-14 12:40:11 +1100

    fix yeoman issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -248,6 +248,9 @@ module.exports = function (grunt) {
 
         // The following *-min tasks produce minified files in the dist folder
         imagemin: {
+            options: {
+                cache: false
+            },
             dist: {
                 files: [{
                     expand: true,
@@ -329,7 +332,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*',
+                        'styles/bootstrap/{,*/}*.*',
                         'CNAME'
                     ]
                 }]
